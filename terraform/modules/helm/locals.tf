4b657a8038625f87d45d9ae0ca2668a5c037c518
Paul Schroeder <paul.schroeder@nimbix.net>
2021-04-26 18:34:48 -0500

    jarvice_ingress_values is no longer needed..

--- terraform/modules/helm/locals.tf
+++ terraform/modules/helm/locals.tf
@@ -7,53 +7,5 @@ locals {
 
     jarvice_chart_is_dir = local.jarvice_chart_version == null ? false : fileexists("${pathexpand(local.jarvice_chart_version)}/Chart.yaml")
 
-    jarvice_helm_values = merge(
-        yamldecode("XXXdummy: value\n\n${fileexists(var.global["values_file"]) ? file(var.global["values_file"]) : ""}"),
-        yamldecode("XXXdummy: value\n\n${fileexists(var.jarvice["values_file"]) ? file(var.jarvice["values_file"]) : ""}"),
-        yamldecode("XXXdummy: value\n\n${var.global["values_yaml"]}"),
-        yamldecode("XXXdummy: value\n\n${var.jarvice["values_yaml"]}"),
-    )
-
-    jarvice = lookup(local.jarvice_helm_values, "jarvice", null)
-    jarvice_jobs_domain = local.jarvice == null ? "" : <<EOF
-jarvice:
-  XXXdummy: XXXdummyvalue
-  ${lookup(local.jarvice, "JARVICE_JOBS_DOMAIN", null) == null ? "" : "JARVICE_JOBS_DOMAIN: ${local.jarvice["JARVICE_JOBS_DOMAIN"]}"}
-EOF
-
-    jarvice_api = lookup(local.jarvice_helm_values, "jarvice_api", null)
-    jarvice_ingress_api = local.jarvice_api == null ? "" : <<EOF
-jarvice_api:
-  XXXdummy: XXXdummyvalue
-  ${lookup(local.jarvice_api, "ingressHost", null) == null ? "" : "ingressHost: ${local.jarvice_api["ingressHost"]}"}
-  ${lookup(local.jarvice_api, "ingressPath", null) == null ? "" : "ingressPath: ${local.jarvice_api["ingressPath"]}"}
-EOF
-
-    jarvice_mc_portal = lookup(local.jarvice_helm_values, "jarvice_mc_portal", null)
-    jarvice_ingress_mc_portal = local.jarvice_mc_portal == null ? "" : <<EOF
-jarvice_mc_portal:
-  XXXdummy: XXXdummyvalue
-  ${lookup(local.jarvice_mc_portal, "ingressHost", null) == null ? "" : "ingressHost: ${local.jarvice_mc_portal["ingressHost"]}"}
-  ${lookup(local.jarvice_mc_portal, "ingressPath", null) == null ? "" : "ingressPath: ${local.jarvice_mc_portal["ingressPath"]}"}
-EOF
-
-    jarvice_k8s_scheduler = lookup(local.jarvice_helm_values, "jarvice_k8s_scheduler", null)
-    jarvice_ingress_k8s_scheduler = local.jarvice_k8s_scheduler == null ? "" : <<EOF
-jarvice_k8s_scheduler:
-  XXXdummy: XXXdummyvalue
-  ${lookup(local.jarvice_k8s_scheduler, "ingressHost", null) == null ? "" : "ingressHost: ${local.jarvice_k8s_scheduler["ingressHost"]}"}
-  ${lookup(local.jarvice_k8s_scheduler, "ingressPath", null) == null ? "" : "ingressPath: ${local.jarvice_k8s_scheduler["ingressPath"]}"}
-EOF
-
-    jarvice_ingress_values = <<EOF
-# local.jarvice_ingress_values
-${local.jarvice_jobs_domain}
-
-${local.jarvice_ingress_api}
-
-${local.jarvice_ingress_mc_portal}
-
-${local.jarvice_ingress_k8s_scheduler}
-EOF
 }
 
