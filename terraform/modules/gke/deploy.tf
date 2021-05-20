4aa811b9dcf2d3fc927eac16e499e62c9af0edeb
Paul Schroeder <paul.schroeder@nimbix.net>
2021-05-19 21:47:28 -0500

    JAR-4279 - cap external dns service account id at 30 chars

--- terraform/modules/gke/deploy.tf
+++ terraform/modules/gke/deploy.tf
@@ -12,7 +12,7 @@ module "common" {
 }
 
 resource "google_service_account" "external_dns" {
-    account_id = "${var.cluster.meta["cluster_name"]}-external-dns"
+    account_id = substr("${var.cluster.meta["cluster_name"]}-external-dns", 0, 30)
     display_name = substr("JARVICE ExternalDNS service account for GKE cluster: ${var.cluster.meta["cluster_name"]}", 0, 100)
 }
 
