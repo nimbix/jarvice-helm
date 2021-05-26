05b45ab69359bb9e9e982650a202e605930d85f6
Paul Schroeder <paul.schroeder@nimbix.net>
2021-05-26 13:41:14 -0500

    Update requires providers for eks

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -4,10 +4,13 @@ terraform {
     required_providers {
         aws = "~> 3.21.0"
 
-        null = "~> 3.0.0"
-        local = "~> 2.0.0"
+        helm = "~> 2.1.2"
+        kubernetes = "~> 2.1.0"
+
+        null = "~> 3.1.0"
+        local = "~> 2.1.0"
         template = "~> 2.2.0"
-        random = "~> 3.0.0"
+        random = "~> 3.1.0"
     }
 }
 
