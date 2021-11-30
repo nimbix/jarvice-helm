0b37d988f7727a9f727aef617c63a79e4285babc
Paul Schroeder <paul.schroeder@nimbix.net>
2021-11-29 20:14:28 -0600

    traefik upgrade won't work unless we set a specific version for it

--- terraform/modules/helm/main.tf
+++ terraform/modules/helm/main.tf
@@ -97,6 +97,7 @@ resource "helm_release" "traefik" {
     name = "traefik"
     repository = "https://helm.traefik.io/traefik"
     chart = "traefik"
+    version = "10.6.2"
     namespace = "kube-system"
     reuse_values = false
     reset_values = true
