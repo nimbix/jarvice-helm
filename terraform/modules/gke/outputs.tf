c8809e17143c290b9fc883a88d9d86f03ef50a06
Kenneth Hill <ken.hill@nimbix.net>
2022-09-26 18:04:11 -0500

    Use kubectl exec plugin for GKE after k8s 1.26
    Signed-off-by: Kenneth Hill <ken.hill@nimbix.net>

--- terraform/modules/gke/outputs.tf
+++ terraform/modules/gke/outputs.tf
@@ -1,10 +1,16 @@
 # outputs.tf - GKE module outputs
-
-resource "local_file" "kube_config" {
-    filename = pathexpand(local.kube_config["config_path"])
-    file_permission = "0600" 
-    directory_permission = "0775"
-    content  = <<EOF
+# use kubectl auth plugin after auth-provider is removed in k8s 1.26
+locals {
+  version_a = split(".", var.cluster.meta["kubernetes_version"])
+  version_b = split(".", "1.26.0")
+  version-test = [
+    for i, j in reverse(range(length(local.version_a)))
+    : signum(local.version_b[i] - tonumber(local.version_a[i])) * pow(10, j)
+  ]
+  # The version-compare value is -1, 0, or 1 if version_a is greater than, equal to or less than version_b respectively
+  version-compare = signum(sum(local.version-test))
+  a-less-than-b   = 1 == local.version-compare
+  auth_kubeconfig = <<EOF
 apiVersion: v1
 kind: Config
 preferences:
@@ -32,6 +38,39 @@ users:
         token-key: '{.credential.access_token}'
       name: gcp
 EOF
+  exec_kubeconfig = <<EOF
+apiVersion: v1
+kind: Config
+preferences:
+  colors: true
+current-context: ${google_container_cluster.jarvice.name}
+contexts:
+- context:
+    cluster: ${google_container_cluster.jarvice.name}
+    namespace: default
+    user: ${google_container_cluster.jarvice.name}
+  name: ${google_container_cluster.jarvice.name}
+clusters:
+- cluster:
+    server: ${local.kube_config["host"]}
+    certificate-authority-data: ${local.kube_config["cluster_ca_certificate"]}
+  name: ${google_container_cluster.jarvice.name}
+users:
+- name: ${google_container_cluster.jarvice.name}
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: gke-gcloud-auth-plugin
+      provideClusterInfo: true
+
+EOF
+}
+
+resource "local_file" "kube_config" {
+    filename = pathexpand(local.kube_config["config_path"])
+    file_permission = "0600"
+    directory_permission = "0775"
+    content  = local.a-less-than-b == true ? local.auth_kubeconfig : local.exec_kubeconfig
 }
 
 output "kube_config" {
